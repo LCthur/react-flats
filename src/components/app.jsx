6cc2a6ec62e7e55be0e62ede0090a284eba62f5f
Loic Thurre <loic.thurre@gmail.com>
2019-07-10 14:25:06 +0200

    google map not working and no credit card

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import FlatList from './flat_list';
 import flats from '../../data/flats';
+import SimpleMap from './map';
 
 const App = () => {
   return (
+  <div>
     <div>
       <FlatList flats={flats} />
     </div>
+    <div className="map-container">
+      <SimpleMap />
+    </div>
+  </div>
   );
 };
 
